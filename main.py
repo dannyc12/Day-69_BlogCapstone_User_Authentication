7131f531a0b0c584849749b4c1f0c8b145dd21dc
Danny Clynes <danny.clynes12@gmail.com>
2022-08-28 19:07:50 -0600

    Add .gitignore and secure key w/env variable

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 import flask_gravatar
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
